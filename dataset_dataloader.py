b04b098114d5febb30cc1c65849d7848563078cf
Ayush Tripathi <ayushtr0904@gmail.com>
2022-07-26 14:15:42 -0700

    Lesson till 10

---
+++ dataset_dataloader.py
@@ -0,0 +1,62 @@
+# '''
+# Epoch = 1 Forward and Backward Pass of all training samples
+
+# Batch_Size = number of training samples in one forward and backward pass
+
+# Number of Iterations = Number of passes, each pass using [batch_size] number of samples
+
+# e.g. 100 samples, batch_size = 20 --> 100/20 = 5 iterations for 1 epoch
+# '''
+
+import math
+import datasets
+import numpy as np
+import torch 
+import torchvision
+from torch.utils.data import Dataset,DataLoader
+
+class WineDataset(Dataset):
+
+        def __init__(self):
+            #data loading '', delimiter= "," dtype = np.float32,skiprows = 1
+            xy = np.loadtxt('./dataset/wine.csv', delimiter = "," , dtype = np.float32, skiprows = 1)
+            self.x = torch.from_numpy(xy[:,1:])
+            self.y = torch.from_numpy(xy[:,[0]])
+            self.n_samples = xy.shape[0]
+           
+
+        def __getitem__(self,index):
+            #dataset[0]
+            return self.x[index], self.y[index]
+            
+
+        def __len__(self):
+            #len dataset
+            return self.n_samples
+        
+
+dataset = WineDataset()
+dataloader = DataLoader(dataset=dataset, batch_size=4, shuffle=True, num_workers=0)
+
+# convert to an iterator and look at one random sample
+# dataiter = iter(dataloader)
+# data = dataiter.next()
+# features,labels = data
+# print(features , labels)
+
+num_epochs = 2
+total_samples = len(dataset)
+n_iterations = math.ceil(total_samples/4)
+print(total_samples,n_iterations)
+
+
+for epoch in range(num_epochs):
+    for i, (inputs,labels) in enumerate(dataloader):
+        # forward backward , update
+        if (i+1) % 5 == 0:
+            print(f'epoch {epoch+1}/{num_epochs}, step {i+1} / {n_iterations}, inputs {inputs.shape}')
+
+
+
+
+
